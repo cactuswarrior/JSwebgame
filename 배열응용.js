a56b79eb2c34b6cab04093e19af1b79ff12893d7
천수승 <cheonss2357@gmail.com>
2023-05-01 16:39:17 +0900

    23.05.01 끝말잇기 임시저장

--- 배열응용.js
+++ 배열응용.js
@@ -1,8 +1,13 @@
 const arr = ['가', '나', '다', '라', '마', '라']
-while (true) {
-  let i = arr.indexOf('라')
-  if (i === -1) break
-  console.log(i)
-  arr.splice(i,1)
+// while (true) {
+//   let i = arr.indexOf('라')
+//   if (i === -1) break
+//   console.log(i)
+//   arr.splice(i,1)
+// }
+// console.log(arr)
+console.log(arr)
+while (arr.indexOf('라')> -1) {
+  arr.splice(arr.indexOf('라'), 1)
 }
 console.log(arr)
---
+++ 함수.js
@@ -0,0 +1,6 @@
+const multi = (x,y,z) => {
+  return x*y*z
+}
+
+const a = multi(4,4,3)
+console.log(a)
